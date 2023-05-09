f23b1718583b81bb6fb1f9b1df1020198f2d6786
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-09 00:39:14 -0500

    home responsive

--- src/views/AboutMe.jsx
+++ src/views/AboutMe.jsx
@@ -7,9 +7,9 @@ export const AboutMe = () => {
     return (
         <div className="w-full flex sm:justify-between bg-white py-16 flex-wrap">
             <div className="w-9/12 m-auto sm:w-1/2 flex justify-center items-center">
-                <img src="/me4.png" alt="" />
+                <img src="/me4.png" alt="me" className="sm:max-w-screen-sm" />
             </div>
-            <div className="w-9/12 m-auto sm:w-1/2 h-full flex items-start flex-col my-auto">
+            <div className="w-9/12 m-auto max-w-screen-sm sm:w-1/2 h-full flex items-start flex-col my-auto">
                 <p className="text-3xl text-blue-dark font-bold my-3">Sobre mi</p>
                 <p className="text-base text-blue-king mr-5 text-justify">Soy ingeniero mecatrónico con más de 2
                     años de experiencia trabajando en el
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -4,22 +4,23 @@ import { Card } from "./components/Card"
 export const HomePage = () => {
     const imageUrl = '/meSmall2.jpg'
     return (
-        <div className="container mx-auto px-4 flex justify-between rounded-2xl mt-16 py-28">
-            <div className="w-1/2 flex justify-center items-start flex-col font-bold">
-                <p className="text-3xl text-blue-light-2">Bienvenidos</p>
-                <p className="text-xl text-white">Mi nombre es Numar Stiven Coy Paez</p>
-                <p className="text-xl text-white">y soy desarrollador <span className="text-blue ">FRONTEND</span></p>
-                <div className="text-white text-2xl flex justify-start my-4">
-                    <RedSocialIcons />
+        <>
+            <div className="container mx-auto px-4 flex sm:justify-between rounded-2xl mt-16 py-28 flex-wrap">
+                <div className="w-9/12 sm:w-1/2 flex justify-center items-start flex-col font-bold ">
+                    <p className="font-mono text-3xl text-blue-light-2">Bienvenidos</p>
+                    <p className="font-mono text-xl text-white">Mi nombre es Numar Stiven Coy Paez</p>
+                    <p className="font-mono text-xl text-white">y soy desarrollador <span className="text-blue font-black">FULL STACK</span></p>
+                    <div className="text-white text-2xl flex justify-start my-4">
+                        <RedSocialIcons />
+                    </div>
+                    <a href="/CV_NUMAR-COY.pdf" target='_blank' download className="bg-blue-light shadow-md shadow-blue p-3 rounded-3xl text-blue-dark my-3">
+                        Descargar CV
+                    </a>
                 </div>
-                <a href="/CV_NumarCoy_spanish.pdf" target='_blank' download className="bg-blue-light shadow-md shadow-blue p-3 rounded-3xl text-blue-dark my-3">
-                    Descargar CV
-                </a>
+                <div className="hidden sm:flex w-1/2 h-full justify-center items-center">
+                    <Card url={imageUrl} />
+                </div>
             </div>
-
-            <div className="w-1/2 h-full flex justify-center items-center">
-                <Card url={imageUrl} />
-            </div>
-        </div>
+        </>
     )
 }

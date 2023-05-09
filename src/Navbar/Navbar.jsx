317434c88d39bf24529f8f4d0465ce88f9685bf5
Stiven Coy <77942755+numarCoy98@users.noreply.github.com>
2023-05-08 23:51:16 -0500

    Menu responsive

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -22,9 +22,13 @@ const listWindowsEng = [
 
 const lenguages = { esp: listWindowsEsp, eng: listWindowsEng }
 
+
 export const Navbar = () => {
 
     const [lenguage, setLenguage] = useState('esp')
+    const [showDropdown, setShowDropdown] = useState(false)
+
+
     return (
         <nav className="flex justify-between items-center h-16 p-5 bg-blue-dark text-lg font-medium fixed w-full top-0">
             <div >
@@ -38,12 +42,25 @@ export const Navbar = () => {
                         duration={500}
                         className="mx-2 text-white transition duration-200 delay-150 hover:text-blue-light cursor-pointer"
                         key={id}>{name.toUpperCase()}
+
                     </Link>))}
                 </ol>
             </div>
-            <div className=" sm:hidden">
-                <div className="text-white text-2xl">
+            <div className="sm:hidden">
+                <div className="text-white text-2xl" onClick={() => setShowDropdown(n => !n)}>
                     <i className='bx bx-menu'></i>
+                </div>
+                <div className={`${showDropdown ? '' : 'hidden'} absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}>
+                    <div className="py-1">
+                        {lenguages[lenguage].map(({ name, id, to }) =>
+                        (<Link to={to}
+                            onClick={() => setShowDropdown(false)}
+                            smooth={true}
+                            duration={500}
+                            className="text-gray-700 block px-4 py-2 text-sm cursor-pointer"
+                            key={id}>{name.toUpperCase()}
+                        </Link>))}
+                    </div>
                 </div>
             </div>
 
